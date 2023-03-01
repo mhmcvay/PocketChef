95e3de2ddfc2e979f79a77514651669a66c5f46d
Max_McVay <mhmcvay@csuchico.edu>
2023-03-01 11:13:51 -0800

    frontend WIP

--- src/content.js
+++ src/content.js
@@ -1,5 +1,28 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.input_value) {
-    console.log("Received message: " + request.input_value);
-  }
+console.log('content')
+// Listen for form submit event
+document.querySelector('form').addEventListener('submit', function(event) {
+  // Prevent the form from submitting
+  event.preventDefault();
+
+  // Retrieve the form values
+  const name = document.querySelector('#nameInput').value;
+  const email = document.querySelector('#emailInput').value;
+  const message = document.querySelector('#messageInput').value;
+
+  // Show an alert with the form values
+  alert(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+});
+
+// Listen for dropdown item click event
+document.querySelectorAll('.dropdown-item').forEach(function(item) {
+  item.addEventListener('click', function(event) {
+    // Prevent the default link behavior
+    event.preventDefault();
+
+    // Retrieve the selected option text
+    const optionText = event.target.textContent;
+
+    // Show an alert with the selected option
+    alert(`Selected option: ${optionText}`);
+  });
 });
