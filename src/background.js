b4d4cb9722ee2fb0116e57f902138f9c2f674dc1
Max_McVay <mhmcvay@csuchico.edu>
2023-03-11 14:07:12 -0800

    added atbash cypher

--- src/background.js
+++ src/background.js
@@ -1,28 +1,45 @@
 // background.js
+//import puppeteer from 'puppeteer-core';
 
 chrome.runtime.onInstalled.addListener(() => {
   //create context menu
   chrome.contextMenus.create({
-      id: "submit-text",
+      id: "submit-magic",
       title: "Magic on: \"%s\" ", 
       contexts: ["selection"], 
   })
+});
 
-  /*
-  chrome.contextMenus.create({
-  title: "Sub-Menu Item 1",
-  parentId: "submit-text",
-  id: "submenu1",
-  contexts: ["all"]
-});
-  */
-});
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
     const inputValue = info.selectionText;
     console.log(inputValue);
     const encrypt = btoa(inputValue);
-    var baseURL = "https://gchq.github.io/CyberChef/#recipe=Magic(3,false,false,'')&input=";
-    var newURL = baseURL + encrypt.replace(/=*$/, '');
-    chrome.tabs.create({ url: newURL});
+      var baseURL = "https://gchq.github.io/CyberChef/#recipe=Magic(3,false,false,'')&input=";
+      var newURL = baseURL + encrypt.replace(/=*$/, '');
+      chrome.tabs.create({ url: newURL});
 });
+
+
+//Experimental
+/*
+chrome.contextMenus.onClicked.addListener(async function(info, tab) {
+  const inputValue = info.selectionText;
+  console.log(inputValue);
+  const encrypt = btoa(inputValue);
+  const baseURL = "https://gchq.github.io/CyberChef/#recipe=Magic(3,false,false,'')&input=";
+  const newURL = baseURL + encrypt.replace(/=*$/, '');
+  chrome.tabs.create({ url: newURL });
+
+  const browser = await puppeteer.launch({
+    executablePath: '~/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    headless: true,
+  });
+  const page = await browser.newPage();
+  await page.goto(newURL);
+  await page.click('#output-format-text');
+  const output = await page.$eval('#output-text', el => el.value);
+  console.log(output);
+  await browser.close();
+});
+*/
